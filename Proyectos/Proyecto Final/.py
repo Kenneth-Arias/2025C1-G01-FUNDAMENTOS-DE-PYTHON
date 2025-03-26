57f7323ff22aa12c8d6a7cae3fe95ac51239f2d8
Kenneth-Arias <alanarias2002@yahoo.com>
2025-03-26 14:56:32 -0600

    Update .py

--- Proyectos/Proyecto Final/.py
+++ Proyectos/Proyecto Final/.py
@@ -1,7 +1,8 @@
 import json
 
-with open('proyecto.json', encoding='utf-8') as archivo:
+with open('proyecto.json') as archivo:
     inventario = json.load(archivo)
+    print('Datos de los pacientes cargados con éxito!\n')
     
 for producto in inventario:
     print(producto)
