1b9c20a714b42d2b9643b4a4f58c61c263fb6037
Kenneth-Arias <alanarias2002@yahoo.com>
2025-03-26 14:59:04 -0600

    Delete .py

--- Proyectos/Proyecto Final/.py
+++
@@ -1,8 +0,0 @@
-import json
-
-with open('proyecto.json') as archivo:
-    inventario = json.load(archivo)
-    print('Datos de los pacientes cargados con éxito!\n')
-    
-for producto in inventario:
-    print(producto)
