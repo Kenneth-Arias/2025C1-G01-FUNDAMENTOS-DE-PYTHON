e0589fec3e267d18f4ab96aad0e3be43911612ee
Kenneth-Arias <alanarias2002@yahoo.com>
2025-03-26 16:41:57 -0600

    Update proyecto.py

--- Proyectos/Proyecto Final/proyecto.py
+++ Proyectos/Proyecto Final/proyecto.py
@@ -1,8 +1,38 @@
 import json
 
-with open('proyecto.json') as archivo:
+with open(r"C:\Users\Admin\OneDrive\Documents\GitHub\2025C1-G01-FUNDAMENTOS-DE-PYTHON\Proyectos\Proyecto Final\proyecto.json") as archivo:
     inventario = json.load(archivo)
-    print('Datos de los pacientes cargados con éxito!\n')
-    
+
 for producto in inventario:
-    print(producto)
+    nombre = producto[0]
+    cantidad = producto[1]
+    print(f"Producto: {nombre}, Cantidad: {cantidad}")
+    
+if input("Desea agregar o modificar el inventario? SI o NO? ").upper == "SI":
+    
+    producto = input("Ingrese el nombre del producto: ")
+    cantidad = int(input("Ingrese la cantidad: "))
+    
+    for producto in inventario:
+        if producto[0].lower() == producto.lower():
+            producto[1] += cantidad  # Sumar la cantidad existente
+            encontrado = True
+            break
+
+    if not encontrado:
+        inventario.append([producto, cantidad])
+
+for producto, cantidad in inventario:
+        if cantidad >= 50:
+            (f"OVERSTOCK: {producto} tiene {cantidad} unidades.")
+        elif cantidad < 10:
+            (f"STOCK BAJO: {producto} tiene solo {cantidad} unidades.")
+        else:
+            print(" No hay alertas de inventario.")
+        
+        
+for producto in inventario:
+    nombre = producto[0]
+    cantidad = producto[1]
+    print(f"Producto: {nombre}, Cantidad: {cantidad}")
+
