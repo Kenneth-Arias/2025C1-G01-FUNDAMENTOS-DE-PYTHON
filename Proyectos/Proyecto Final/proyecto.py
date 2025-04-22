c683ddf96640c1bd71943a6a2263de32daea6afb
Kenneth-Arias <alanarias2002@yahoo.com>
2025-04-22 08:08:19 -0600

    Update proyecto.py

--- Proyectos/Proyecto Final/proyecto.py
+++ Proyectos/Proyecto Final/proyecto.py
@@ -1,35 +1,11 @@
-import json
+#Proyecto de Kenneth Arias Solís 
+#Sistema de organizacion de inventario
 
-with open(r"C:\Users\Admin\OneDrive\Documents\GitHub\2025C1-G01-FUNDAMENTOS-DE-PYTHON\Proyectos\Proyecto Final\proyecto.json") as archivo:
-    inventario = json.load(archivo)
 
-for producto in inventario:
-    nombre = producto[0]
-    cantidad = producto[1]
-    print(f"Producto: {nombre}, Cantidad: {cantidad}")
 
-print("Agregar articulo: ")
-if input("Desea agregar o modificar el inventario? SI o NO? ").upper == "SI":
+
+
+
+
+while(True):
     
-    productoN = input("Ingrese el nombre del producto: ")
-    cantidadN = int(input("Ingrese la cantidad: "))
-    
-    for producto in inventario:
-        if producto[0].lower() == productoN.lower():
-            producto[1] += cantidadN
-            break
-
-for producto, cantidad in inventario:
-        if cantidad >= 50:
-            (f"OVERSTOCK: {producto} tiene {cantidad} unidades.")
-        elif cantidad < 10:
-            (f"STOCK BAJO: {producto} tiene solo {cantidad} unidades.")
-        else:
-            print(" No hay alertas de inventario.")
-        
-        
-for producto in inventario:
-    nombre = producto[0]
-    cantidad = producto[1]
-    print(f"Producto: {nombre}, Cantidad: {cantidad}")
-
